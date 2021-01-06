b7aacb12ff40aefc4996d4da9c6632c1ec628f8a
Abhay Cashikar <acashikar6@gatech.edu>
2021-01-06 17:12:27 -0600

    Added Reddit API calls to get sub details

--- bot.py
+++ bot.py
@@ -2,14 +2,21 @@
 import os
 import re
 import discord
+from discord.errors import NotFound
+import praw
 from discord import colour
 from dotenv import load_dotenv
 
 load_dotenv('./.env')
-TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('MY_GUILD')
 
 client = discord.Client()
+reddit = praw.Reddit(
+    client_id=os.getenv('REDDIT_ID'),
+    client_secret=os.getenv('REDDIT_SECRET'),
+    user_agent="discord:subredditlinker:v1.0.0 (by u/approvedcargo91)"
+)
 
 @client.event
 async def on_ready():
@@ -22,11 +29,21 @@ async def on_ready():
 async def on_message(message: discord.Message):
     if message.author == client.user:
         return
-    matches = re.findall('(?<=r\/)[0-9A-Za-z][0-9A-Za-z_]*(?= |$)', message.content)
+    matches = re.findall('(?<!\/)r\/[0-9A-Za-z][0-9A-Za-z_]*(?= |$)', message.content)
     if matches:
         bot_message = ''
         for match in matches:
-            bot_message += '[' + match + '](https://www.reddit.com/r/' + match + ')\n'
-        await message.channel.send(embed=discord.Embed(title='Linked Subreddits', description=bot_message[:-1], color=colour.Color(0xFF4500)))
+            name = match[2:]
+            try:
+                subreddit = reddit.subreddit(name)
+                desc: str = subreddit.public_description
+                if desc.find('\n') in range(200):
+                    desc = desc[:desc.find('\n')]
+                elif len(desc) > 203:
+                    desc = desc[:200] + '...'
+                bot_message += '\n[' + name + '](https://www.reddit.com/r/' + name + ')\n' + desc + '\n'
+            except:
+                bot_message += '\n' + name + ' does not exist. Or maybe it\'s private?'
+        await message.channel.send(embed=discord.Embed(title='Linked Subreddits', description=bot_message[1:-1], color=colour.Color(0xFF4500)))
 
-client.run(TOKEN)
+client.run(DISCORD_TOKEN)
