57a35881a66abd6d96910aacca26bbc28ba0be4b
Abhay Cashikar <acashikar6@gatech.edu>
2021-01-07 17:03:26 -0600

    added ａｅｓｔｈｅｔｉｃｓ

--- bot.py
+++ bot.py
@@ -6,10 +6,15 @@ from discord.errors import NotFound
 import praw
 from discord import colour
 from dotenv import load_dotenv
+import prawcore
 
 load_dotenv('./.env')
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('MY_GUILD')
+ORANGE = colour.Color(0xFF4500)
+PFP = 'https://github.com/abhaycashikar/SubredditLinker/raw/main/pfp.png'
+FOOTER = 'Found an issue? Tag the bot to get the GitHub link!'
+FOOTER_ICON = 'https://github.com/abhaycashikar/SubredditLinker/raw/main/question_mark.png'
 
 client = discord.Client()
 reddit = praw.Reddit(
@@ -29,21 +34,64 @@ async def on_ready():
 async def on_message(message: discord.Message):
     if message.author == client.user:
         return
+    
+    if client.user in message.mentions:
+        embed = discord.Embed(
+            title='SubredditLinker',
+            url='https://github.com/abhaycashikar/SubredditLinker',
+            description='Links to and displays basic info for subreddit names (i.e. "r/" followed by a string) in user messages.',
+            color=ORANGE
+        )
+        embed.set_thumbnail(url=PFP)
+        embed.add_field(name='Found an issue?', value='[Let me know!](https://github.com/abhaycashikar/SubredditLinker/issues)')
+        await message.channel.send(embed=embed)
+
     matches = re.findall('(?<!\/)r\/[0-9A-Za-z][0-9A-Za-z_]*(?= |$)', message.content)
     if matches:
-        bot_message = ''
+        maxed_matches = False
+        if len(matches) > 5:
+            matches = matches[:5]
+            maxed_matches = True
+
         for match in matches:
-            name = match[2:]
             try:
-                subreddit = reddit.subreddit(name)
+                subreddit = reddit.subreddit(match[2:])
                 desc: str = subreddit.public_description
                 if desc.find('\n') in range(200):
                     desc = desc[:desc.find('\n')]
                 elif len(desc) > 203:
                     desc = desc[:200] + '...'
-                bot_message += '\n[' + name + '](https://www.reddit.com/r/' + name + ')\n' + desc + '\n'
-            except:
-                bot_message += '\n' + name + ' does not exist. Or maybe it\'s private?'
-        await message.channel.send(embed=discord.Embed(title='Linked Subreddits', description=bot_message[1:-1], color=colour.Color(0xFF4500)))
+                embed = discord.Embed(
+                    title=match,
+                    url='https://reddit.com/r/' + match,
+                    description=desc,
+                    color=ORANGE)
+                embed.set_thumbnail(url=subreddit.icon_img if subreddit.icon_img else subreddit.community_icon)
+                embed.set_footer(text=FOOTER, icon_url=FOOTER_ICON)
+                post_links = []
+                for post in subreddit.top(limit=3):
+                    post_links.append('[' + post.title + '](' + post.url + ')')
+                embed.add_field(name='Top Posts of All Time', value='\n'.join(post_links))
+                await message.channel.send(embed=embed)
+            except prawcore.exceptions.Forbidden:
+                embed = discord.Embed(
+                    title=match,
+                    url='https://reddit.com/r/' + match, 
+                    description='Could not fetch details because ' + match + ' is either a private or quarantined subreddit.',
+                    color=ORANGE
+                )
+                embed.set_footer(text=FOOTER, icon_url=FOOTER_ICON)
+                await message.channel.send(embed=embed)
+            except prawcore.exceptions.NotFound:
+                embed = discord.Embed(
+                    title=match,
+                    description=match + ' is either a banned subreddit, or it does not exist.',
+                    color=ORANGE
+                )
+                embed.set_footer(text=FOOTER, icon_url=FOOTER_ICON)
+                await message.channel.send(embed=embed)
+
+        if maxed_matches:
+            await message.channel.send(content='Can only link five subreddits at a time.')
 
 client.run(DISCORD_TOKEN)
