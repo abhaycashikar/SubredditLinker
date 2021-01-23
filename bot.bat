27a5879a4225e2ec53623eb3b2d47c757a53239a
Abhay Cashikar <acashikar6@gatech.edu>
2021-01-23 13:10:06 -0500

    Added Windows bat script

---
+++ bot.bat
@@ -0,0 +1,3 @@
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
+cd "C:\Users\Abhay Cashikar\OneDrive - Georgia Institute of Technology\Documents\SubredditLinker"
+"C:\Users\Abhay Cashikar\AppData\Local\Programs\Python\Python39-32\python.exe" bot.py
